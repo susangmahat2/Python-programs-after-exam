55a6027feda2d0aaebaab8b177d1ec9a055fd904
susangmahat2 <susangmahat2@gmail.com>
2026-03-28 20:33:38 +0900

    Add files via upload

---
+++ reverselist.py
@@ -0,0 +1,12 @@
+emptylist=[]
+print()
+number=[1,2,3,4,5]
+print("Numbers list:", number)
+
+triple=[1,2,3]*3
+print("Triples list:", triple)
+
+alist=[100, 200, 300, 400, 500]
+alist=alist[::-1]
+print("Reveresed list:", alist)
+print()
