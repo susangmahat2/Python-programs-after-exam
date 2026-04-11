11615ac654845f3cf09ec2ed2ac54ecf33b3d728
susangmahat2 <susangmahat2@gmail.com>
2026-04-11 20:32:30 +0900

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,7 @@
+set1={"green","blue"}
+set2={"blue","yellow"}
+
+intersection=set1.intersection(set2)
+print("Set1 :", set1)
+print("Set2 :", set2)
+print("Intersection of set1 and set2 :", intersection)
