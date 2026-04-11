b97680a6280bf4bb7ddba9410edd629c34285a3f
susangmahat2 <susangmahat2@gmail.com>
2026-04-11 20:26:35 +0900

    Add files via upload

---
+++ operationofsets.py
@@ -0,0 +1,14 @@
+my_set={1,2,3}
+print(my_set)
+
+my_set={1.0, "Hello", (1,2,3)}
+print(my_set)
+
+my_set={1,2,3,4,3,2}
+print(my_set)
+
+num_set=set([0,1,2,3,4])
+print("Oringinal set:", num_set)
+num_set.pop()
+print("After eleminating 1st element:")
+print(num_set)
