961bbc1cf750027108975969864d2ebe853bdce1
susangmahat2 <susangmahat2@gmail.com>
2026-04-19 20:42:01 +0900

    Add files via upload

---
+++ classvechile.py
@@ -0,0 +1,8 @@
+class vechile:
+    def __init__(self, max_speed, mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+modelX=vechile(240, 18)
+print("The max speed of the car is:", modelX.max_speed)
+print("The mileage of the car is:", modelX.mileage)
