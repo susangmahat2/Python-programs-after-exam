7c3ad59f4eaf715c0eb9b498fd982c015eba3f02
susangmahat2 <susangmahat2@gmail.com>
2026-04-24 20:37:04 +0900

    Add files via upload

---
+++ ios.py
@@ -0,0 +1,10 @@
+class IOString:
+    def __init__(self):
+        self.str1=""
+    def get_string(self):
+        self.str1=input("Enter a string: ")
+    def print_String(self):
+        print("Result: ",self.str1.upper())
+str1=IOString()
+str1.get_string()
+str1.print_String()
